dd6b30a7680bdec5131d0569727ac95354bf53c4
maakbaas <johannesvg+maakbaas@gmail.com>
2020-07-18 13:51:01 +0200

    Copy config file

--- build/preBuild.py
+++ build/preBuild.py
@@ -1,5 +1,6 @@
 Import('env')
 from os.path import join, realpath
+from shutil import copyfile
 import subprocess
 
 # default setting = rebuild config and do not rebuild HTML or certificates
@@ -15,6 +16,9 @@ for item in env.get("CPPDEFINES", []):
         config = True
     elif item == "REBUILD_CERTS":
         certs = True
+    elif isinstance(item, tuple) and item[0] == "CONFIG_PATH":
+        copyfile(env.get("PROJECT_DIR") + '\\' + item[1], '../html/js/configuration.json')
+
 
 if html:
     subprocess.call("preBuildHTML.py", shell=True)
