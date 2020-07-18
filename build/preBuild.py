51272a574ac29a10f8e2ac3ecdd19dda1db249cd
maakbaas <johannesvg+maakbaas@gmail.com>
2020-07-18 13:17:27 +0200

    Example and CPP defines

--- build/preBuild.py
+++ build/preBuild.py
@@ -1,5 +1,26 @@
+Import('env')
+from os.path import join, realpath
 import subprocess
 
-subprocess.call("preBuildHTML.py", shell=True)
-subprocess.call("preBuildConfig.py", shell=True)
-subprocess.call("preBuildCertificates.py", shell=True)
+# default setting = rebuild config and do not rebuild HTML or certificates
+html = False
+config = True
+certs = False
+
+# private library flags
+for item in env.get("CPPDEFINES", []):
+    if item == "REBUILD_HTML":
+        html = True
+    elif item == "REBUILD_CONFIG":
+        config = True
+    elif item == "REBUILD_CERTS":
+        certs = True
+
+if html:
+    subprocess.call("preBuildHTML.py", shell=True)
+if config:
+    subprocess.call("preBuildConfig.py", shell=True)
+if certs:
+    subprocess.call("preBuildCertificates.py", shell=True)
+
+
