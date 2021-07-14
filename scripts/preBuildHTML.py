ec6b69554f66fd32fbd5f51ed6e93c49671d649d
Fernando Garcia <fefegarcia_1@hotmail.com>
2021-07-14 19:45:09 -0300

    Install webpack before build

--- scripts/preBuildHTML.py
+++ scripts/preBuildHTML.py
@@ -1,4 +1,8 @@
 import os
 
 def preBuildHTMLFun():
+    os.system("npm install")
+    os.system("npm audit fix")
+    os.system("npx browserslist@latest --update-db")
     os.system("npm run build")
+    
