06e16149a91cb97fabb68ad8fefe4c3484ce1287
Fernando Garcia <fefegarcia_1@hotmail.com>
2021-07-15 15:40:37 -0300

    Install webpack automatically (#116)
    * Install webpack automatically
    
    * Changes requested

--- scripts/preBuildHTML.py
+++ scripts/preBuildHTML.py
@@ -1,4 +1,15 @@
-import os
+from subprocess import call
+import os.path
+
 
 def preBuildHTMLFun():
-    os.system("npm run build")
+    webpackInst = os.path.isdir("../node_modules")
+
+    if webpackInst == False:
+        print("Running npm ci...")
+        call(["npm", "ci"])
+        print("Running npx browserslist@latest --update-db...")
+        call(["npx", "browserslist@latest", "--update-db"])
+
+    print("Running npm run build...")
+    call(["npm", "run", "build"])
