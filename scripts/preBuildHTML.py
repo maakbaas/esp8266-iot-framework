626ef95bcce8fb830395745dd85bd82da62236ea
Maakbaas <johannesvg+maakbaas@gmail.com>
2021-11-06 11:37:23 +0100

    Bugfixes to npm ci command

--- scripts/preBuildHTML.py
+++ scripts/preBuildHTML.py
@@ -1,15 +1,17 @@
 from subprocess import call
-import os.path
+import inspect, os.path
 
 
 def preBuildHTMLFun():
-    webpackInst = os.path.isdir("../node_modules")
+    filename = inspect.getframeinfo(inspect.currentframe()).filename
+    dir_path = os.path.dirname(os.path.abspath(filename))
+    webpackInst = os.path.isdir(dir_path + "/../node_modules")
 
     if webpackInst == False:
         print("Running npm ci...")
-        call(["npm", "ci"])
+        call(["npm", "ci"], shell=True)
         print("Running npx browserslist@latest --update-db...")
-        call(["npx", "browserslist@latest", "--update-db"])
+        call(["npx", "browserslist@latest", "--update-db"], shell=True)
 
     print("Running npm run build...")
-    call(["npm", "run", "build"])
+    call(["npm", "run", "build"], shell=True)
