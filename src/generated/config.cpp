9eaeb55587a60897349a632cd8e416e7a519ec83
aaron GitHub <aaronse@outlook.com>
2022-01-26 04:06:16 -0800

    ESP32, take 2

--- src/generated/config.cpp
+++ src/generated/config.cpp
@@ -1,10 +1,25 @@
 #include <Arduino.h>
 #include "config.h"
 
-uint32_t configVersion = 3113778868; //generated identifier to compare config with EEPROM
+uint32_t configVersion = 3764343299; //generated identifier to compare config with EEPROM
 
 const configData defaults PROGMEM =
 {
-	"ESP8266 IoT Framework",
-	"en"
+	"Haptic-01",
+	"Haptic Light ESP8266",
+	"Aurora",
+	50,
+	20,
+	true,
+	true,
+	"#ff00ff",
+	23,
+	50,
+	true,
+	60,
+	true,
+	false,
+	30,
+	1.2345,
+	"invisible!"
 };
--- src/generated/config.h
+++ src/generated/config.h
@@ -3,8 +3,23 @@
 
 struct configData
 {
+	char hostName[32];
 	char projectName[32];
-	char language[3];
+	char mode[20];
+	uint8_t brightness;
+	uint8_t nightBrightness;
+	bool isActive;
+	bool isPlaying;
+	char color[11];
+	uint8_t speed;
+	uint8_t audioLevel;
+	bool isPirSensorEnabled;
+	uint16_t pirActiveTimeoutSecs;
+	bool isLuxSensorEnabled;
+	bool isTempSensorEnabled;
+	uint16_t pixelCount;
+	float dummyFloat;
+	char dummyString[11];
 };
 
 extern uint32_t configVersion;
