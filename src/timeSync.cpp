baf7c7cfe31538266b5e9ba756056d5883863625
aaron GitHub <aaronse@outlook.com>
2022-09-25 21:57:33 -0700

    ESP32 'Implementation' for timeSync.cpp, requires posix timezone expressions, basically just pass thru to synchronous configTzTime.  The Async logic isn't used for ESP32

--- src/timeSync.cpp
+++ src/timeSync.cpp
@@ -76,10 +76,16 @@ int8_t NTPSync::waitForSyncResult(unsigned long timeoutLengthMs)
 void NTPSync::setTime(const char* tz, const char* server1, const char* server2, const char* server3)
 {
 #ifdef ESP32
-    // TODO: Implement ESP32 version
+    // configTzTime @ https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/esp32-hal-time.c
+    configTzTime(tz, server1, server2, server3);
+    
+    // Flag as complete, since ESP32 configTzTime executes synchronously.
+    synced = true;
+
 #elif defined(ESP8266)
     settimeofday_cb([this](){ synced = true; });
     configTime(tz, server1, server2, server3);
+
 #endif
 }
 
